7739e8c722a9cdfb796f256793224f673b8ee108
Arch <wbhaoui@gmail.com>
2024-12-30 22:29:46 +0100

    Adjust search bar styling

--- app/_ui/components/MainHeader.tsx
+++ app/_ui/components/MainHeader.tsx
@@ -1,25 +1,25 @@
-import { Bell, Edit, Search } from 'lucide-react'
-import Image from "next/image"
-import Link from "next/link"
+import { Bell, Edit, Search } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
 
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import Logo from './Logo'
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import Logo from "./Logo";
 
-export default function MainHeader () {
+export default function MainHeader() {
   return (
-    <header className="flex items-center justify-between border-b px-4 md:px-6">
+    <header className="flex items-center justify-between border-b py-2 px-4 md:px-6">
       <div className="flex items-center gap-4">
         <Link href="/" className="flex items-center">
-          <Logo className={"text-main bg-white text-2xl"}/>
+          <Logo className={"text-main bg-white text-3xl"} />
         </Link>
         <div className="hidden md:flex relative max-w-md">
-          <Search className="absolute left-2 top-1/2 h-4 w-4 -translate-y-1/2 transform text-muted-foreground" />
           <Input
             type="search"
             placeholder="Search"
-            className="w-[240px] pl-8 bg-muted/50"
+            className="peer w-[240px] pl-8 bg-muted/50 shadow-none border-none focus-visible:ring-0 rounded-full font-semibold placeholder:text-gray-700 placeholder:font-semibold"
           />
+          <Search className="absolute left-2 top-1/2 h-4 w-4 -translate-y-1/2 transform text-gray-700 peer-focus:text-black" />
         </div>
       </div>
       <div className="flex items-center gap-2">
@@ -41,6 +41,5 @@ export default function MainHeader () {
         </Button>
       </div>
     </header>
-  )
+  );
 }
-

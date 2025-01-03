243bcae2b441e12221786e51fc3d368bf445cb38
Arch <wbhaoui@gmail.com>
2025-01-03 18:00:56 +0100

    Fix SSR issue with tiptap editor

--- app/_ui/components/Editor.tsx
+++ app/_ui/components/Editor.tsx
@@ -1,15 +1,24 @@
 'use client'
-import { EditorProvider } from '@tiptap/react'
+import { EditorProvider, UseEditorOptions } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
 import BubbleMenuWrapper from './tiptap/BubbleMenu/BubbleMenu';
+import { HTMLAttributes } from 'react';
 
 const extensions = [StarterKit];
 
 const content = '<p>Hello World!</p>'
 
+const editorProps : UseEditorOptions = {
+  immediatelyRender: false,
+}
+
+const editorContainerProps : HTMLAttributes<HTMLDivElement> = {
+  className: 'tiptap-editor'
+}
+
 const Tiptap = () => {
   return (
-    <EditorProvider extensions={extensions} content={content}>
+    <EditorProvider {...editorProps} editorContainerProps={editorContainerProps} extensions={extensions} content={content}>
       <BubbleMenuWrapper>
 
       </BubbleMenuWrapper>

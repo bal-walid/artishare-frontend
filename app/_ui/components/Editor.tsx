e28722a629a849b009666301202c1faffef97f4e
Arch <wbhaoui@gmail.com>
2025-01-03 20:21:08 +0100

    Change document schema to start with heading and configure placeholders

--- app/_ui/components/Editor.tsx
+++ app/_ui/components/Editor.tsx
@@ -1,30 +1,45 @@
-'use client'
-import { EditorProvider, UseEditorOptions } from '@tiptap/react'
-import StarterKit from '@tiptap/starter-kit'
-import BubbleMenuWrapper from './tiptap/BubbleMenu/BubbleMenu';
-import { HTMLAttributes } from 'react';
+"use client";
+import { EditorProvider, UseEditorOptions } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+import Placeholder from "@tiptap/extension-placeholder";
+import Document from "@tiptap/extension-document"
+import BubbleMenuWrapper from "./tiptap/BubbleMenu/BubbleMenu";
+import { HTMLAttributes } from "react";
 import "@/app/_ui/stylesheets/editor.scss";
 
-const extensions = [StarterKit];
+const HeadingFirstDocument = Document.extend({
+  content: 'heading block*',
+})
 
-const content = '<p>Hello World!</p>'
+const extensions = [
+  StarterKit.configure({document: false}),
+  HeadingFirstDocument,
+  Placeholder.configure({ placeholder: ({node}) => {
+    if (node.type.name === 'heading') {
+      return 'Your story\'s title';
+    }
+    return 'Tell your story'
+  }}),
+];
 
-const editorProps : UseEditorOptions = {
+const editorProps: UseEditorOptions = {
   immediatelyRender: false,
-}
+};
 
-const editorContainerProps : HTMLAttributes<HTMLDivElement> = {
-  className: 'tiptap-editor'
-}
+const editorContainerProps: HTMLAttributes<HTMLDivElement> = {
+  className: "tiptap-editor",
+};
 
 const Tiptap = () => {
   return (
-    <EditorProvider {...editorProps} editorContainerProps={editorContainerProps} extensions={extensions} content={content}>
-      <BubbleMenuWrapper>
-
-      </BubbleMenuWrapper>
+    <EditorProvider
+      {...editorProps}
+      editorContainerProps={editorContainerProps}
+      extensions={extensions}
+    >
+      <BubbleMenuWrapper></BubbleMenuWrapper>
     </EditorProvider>
   );
-}
+};
 
 export default Tiptap;

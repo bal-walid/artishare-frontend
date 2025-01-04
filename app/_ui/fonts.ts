94fbb09a906ff64392a0e61b9e26eccf2f8f0f83
Arch <wbhaoui@gmail.com>
2025-01-04 12:35:29 +0100

    Change mono font to Source Code Pro

--- app/_ui/fonts.ts
+++ app/_ui/fonts.ts
@@ -1,5 +1,5 @@
-import { Playfair_Display_SC, Playfair_Display, Inter, IBM_Plex_Mono } from "next/font/google";
+import { Playfair_Display_SC, Playfair_Display, Inter, Source_Code_Pro } from "next/font/google";
 export const playfairSC = Playfair_Display_SC({weight: ["400", "700", "900"], variable: "--font-playfair-sc"});
 export const playfair = Playfair_Display({weight: ["400", "700", "900"], variable: "--font-playfair"});
 export const inter = Inter({variable: "--font-inter"});
-export const ibmPlexMono = IBM_Plex_Mono({weight: ["400"], variable:"--font-mono"});
+export const sourceCodePro = Source_Code_Pro({weight: ["400"], variable:"--font-mono"});
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import "./globals.css";
-import { playfairSC, playfair, inter, ibmPlexMono } from "./_ui/fonts";
+import { playfairSC, playfair, inter, sourceCodePro } from "./_ui/fonts";
 import { JetBrains_Mono } from "next/font/google";
 
 export default function RootLayout({
@@ -10,7 +10,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${ibmPlexMono.variable} ${playfairSC.variable} ${playfair.variable} ${inter.variable} ${inter.className} antialiased h-dvh flex flex-col overflow-y-auto overflow-x-hidden`}
+        className={`${sourceCodePro.variable} ${playfairSC.variable} ${playfair.variable} ${inter.variable} ${inter.className} antialiased h-dvh flex flex-col overflow-y-auto overflow-x-hidden`}
       >
         {children}
       </body>

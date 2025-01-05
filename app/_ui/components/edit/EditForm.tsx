b6645d9bd079c17bef60a4a8f992e9c7e7aa7545
Arch <wbhaoui@gmail.com>
2025-01-05 11:29:53 +0100

    Add EditForm component

---
+++ app/_ui/components/edit/EditForm.tsx
@@ -0,0 +1,11 @@
+interface EditFormProps {
+  htmlContent : string | undefined;
+}
+
+const EditForm = ({htmlContent} : EditFormProps) => {
+  console.log(htmlContent);
+  return (
+    <div></div>
+  );
+}
+export default EditForm;
--- app/edit/page.tsx
+++ app/edit/page.tsx
@@ -1,18 +1,24 @@
-'use client'
+"use client";
 
 import { useRef, useState } from "react";
 import Tiptap from "../_ui/components/tiptap/Editor";
 import { Editor } from "@tiptap/react";
 import EditHeader from "../_ui/components/tiptap/EditHeader";
 import Modal from "../_ui/components/Modal";
+import EditForm from "../_ui/components/edit/EditForm";
 export default function EditPage() {
   const [modalOpen, setModalOpen] = useState(false);
   const editorRef = useRef<Editor | null>(null);
   return (
     <div className="h-full flex flex-col">
-      <EditHeader onBtnClick={() => setModalOpen(true)}/>
+      <EditHeader onBtnClick={() => setModalOpen(true)} />
       <Tiptap editorRef={editorRef} />
-      <Modal isOpen={modalOpen} onClose={() => setModalOpen(false)}>{""}</Modal>
+      {/* As in, do not allow the modal to render until the editor is ready */}
+      {editorRef.current && (
+        <Modal isOpen={modalOpen} onClose={() => setModalOpen(false)}>
+          <EditForm htmlContent={editorRef.current.getHTML()} />
+        </Modal>
+      )}
     </div>
   );
 }

a3cc2e294d8f622e999ffd1b5e70ab4528394b50
Arch <wbhaoui@gmail.com>
2025-01-06 17:20:02 +0100

    Extract BlogCard component from BlogList

---
+++ app/_ui/components/blogList/BlogCard.tsx
@@ -0,0 +1,75 @@
+import { Card, CardHeader, CardContent } from "@/components/ui/card";
+import { Avatar, AvatarImage } from "@radix-ui/react-avatar";
+import { Button } from "@/components/ui/button";
+import { BookmarkIcon, HeartIcon, MessageCircleIcon } from "lucide-react";
+import formatDate from "@/lib/formatDate";
+import Image from "next/image";
+import { DisplayBlog } from "@/app/_type/blogs";
+
+const BlogCard = ({blog} : {blog: DisplayBlog}) => {
+  return (
+    <Card
+    className="border-0 border-b rounded-none shadow-none"
+  >
+    <CardHeader className="py-4 px-0">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-2">
+          <Avatar className="h-6 w-6 border-2 border-secondary">
+            <AvatarImage
+              src="/avatar_placeholder.png"
+              alt={blog.creator}
+            />
+          </Avatar>
+          <span className="text-xs">{blog.creator}</span>
+        </div>
+      </div>
+    </CardHeader>
+    <CardContent className="p-0 pb-6">
+      <div className="flex  justify-between">
+        <div className="space-y-2 flex-1">
+          <h2 className="text-2xl font-bold leading-tight">
+            {blog.title}
+          </h2>
+          <p className="text-muted-foreground">{blog.description}</p>
+          <div className="flex items-center gap-3 text-xs text-medium-gray">
+            <span>{formatDate(blog.date)}</span>
+            <span className="inline-flex gap-1 items-center">
+              <MessageCircleIcon
+                strokeWidth={0}
+                fill="#6B6B6B"
+                className="h-4 w-4"
+              />
+
+              <span>{blog.commentCount}</span>
+            </span>
+            <span className="inline-flex gap-1 items-center">
+              <HeartIcon
+                strokeWidth={0}
+                fill="#6B6B6B"
+                className="h-4 w-4"
+              />
+
+              <span>{blog.likeCount}</span>
+            </span>
+            <Button
+              className="ml-auto"
+              variant="mediumLike"
+              size="icon"
+            >
+              <BookmarkIcon strokeWidth={1} className="!h-6 !w-6 " />
+            </Button>
+          </div>
+        </div>
+        <Image
+          src="/placeholder.svg"
+          alt="Blog cover image"
+          width={128}
+          height={96}
+          className=" object-cover rounded-sm ml-16"
+        />
+      </div>
+    </CardContent>
+  </Card>
+  );
+}
+export default BlogCard;
--- app/_ui/components/blogList/BlogList.tsx
+++ app/_ui/components/blogList/BlogList.tsx
@@ -1,79 +1,31 @@
 "use client";
 import { DisplayBlog } from "@/app/_type/blogs";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import BlogCard from "./BlogCard";
 import { Button } from "@/components/ui/button";
 import { BookmarkIcon, HeartIcon, MessageCircleIcon } from "lucide-react";
 import formatDate from "@/lib/formatDate";
 import Image from "next/image";
+import { useIntersection } from "@/app/_hooks/useIntersection";
 
 const BlogList = () => {
   const [blogs] = useState(defaultBlogs);
+  const observerRef = useRef<HTMLDivElement | null>(null);
+  const { isVisible } = useIntersection({
+    element: observerRef,
+    rootMargin: "0px",
+  });
   return (
-    <div className="max-w-[728px] p-4">
-      {blogs.map((blog, index) => (
-        <Card
-          key={index}
-          className="border-0 border-b rounded-none shadow-none"
-        >
-          <CardHeader className="py-4 px-0">
-            <div className="flex items-center justify-between">
-              <div className="flex items-center gap-2">
-                <Avatar className="h-6 w-6 border-2 border-secondary">
-                  <AvatarImage
-                    src="/avatar_placeholder.png"
-                    alt={blog.creator}
-                  />
-                </Avatar>
-                <span className="text-xs">{blog.creator}</span>
-              </div>
-            </div>
-          </CardHeader>
-          <CardContent className="p-0 pb-6">
-            <div className="flex  justify-between">
-              <div className="space-y-2 flex-1">
-                <h2 className="text-2xl font-bold leading-tight">
-                  {blog.title}
-                </h2>
-                <p className="text-muted-foreground">{blog.description}</p>
-                <div className="flex items-center gap-3 text-xs text-medium-gray">
-                  <span>{formatDate(blog.date)}</span>
-                  <span className="inline-flex gap-1 items-center">
-                    <MessageCircleIcon
-                      strokeWidth={0}
-                      fill="#6B6B6B"
-                      className="h-4 w-4"
-                    />
-
-                    <span>{blog.commentCount}</span>
-                  </span>
-                  <span className="inline-flex gap-1 items-center">
-                    <HeartIcon
-                      strokeWidth={0}
-                      fill="#6B6B6B"
-                      className="h-4 w-4"
-                    />
-
-                    <span>{blog.likeCount}</span>
-                  </span>
-                  <Button className="ml-auto" variant="mediumLike" size="icon">
-                    <BookmarkIcon strokeWidth={1} className="!h-6 !w-6 " />
-                  </Button>
-                </div>
-              </div>
-              <Image
-                src="/placeholder.svg"
-                alt="Blog cover image"
-                width={128}
-                height={96}
-                className=" object-cover rounded-sm ml-16"
-              />
-            </div>
-          </CardContent>
-        </Card>
-      ))}
-    </div>
+    <>
+      <div className="max-w-[728px] p-4">
+        {blogs.map((blog) => (
+          <BlogCard key={blog.id} blog={blog}/>
+        ))}
+        <div ref={observerRef}></div>
+      </div>
+    </>
   );
 };
 
